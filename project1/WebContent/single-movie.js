ed93fbc6c7fd895fa37f3ccbb4ba94e3afec2cce
ChenLinL <chenlinl@uci.edu>
2019-01-20 15:48:47 -0800

    connect

---
+++ project1/WebContent/single-movie.js
@@ -0,0 +1,95 @@
+/**
+ * Retrieve parameter from request URL, matching by parameter name
+ * @param target String
+ * @returns {*}
+ */
+
+
+//$(window).on('load', $.get("api/movies", {"movie_name": "something"}, function(resultData){handleResult(resultData)} ));
+
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    // Return the decoded parameter value
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function createTheHtml(l1,l2,n){
+	result ='';
+	result += "<th><ul>";
+	num = Number(n);
+	for(i = 0; i<num; i++)
+	{
+		result += "<li><a href=single-star.html?id=" + l1[i] + '>'+ l2[i] + '</a></li>'
+	}
+	result += "</ul></th>";
+	return result;
+
+}
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into html elements
+ * @param resultData jsonObject
+ */
+
+function handleResult(resultData) {
+    // populate the movie info h3
+    // find the empty h3 body by id "movie_info"
+    let movieInfoElement = jQuery("#movie_info");
+
+    console.log("handleResult: populating movie table from resultData");
+    
+    let backInfoElement = jQuery('#Back');
+    backInfoElement.append("<p><a href=index.html>Back to Movie Page</p>")
+
+    // Populate the star table
+    // Find the empty table body by id "movie_table_body"
+    let movieTableBodyElement = jQuery("#single_movie_table_body");
+
+    // Concatenate the html tags with resultData jsonObject to create table rows
+    for (let i = 0; i < Math.min(20, resultData.length); i++) {
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        
+//        resultData[i]["movie_star"][1]
+        	 rowHTML += "<th>" + resultData[i]["movie_id"] + "</th>";
+        	 rowHTML += "<th>" + resultData[i]["movie_title"] + "</th>";
+             rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+             rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+             rowHTML += "<th>" + resultData[i]["movie_genre"] + "</th>";
+             //rowHTML += createTheHtml(resultData[i]["star_id"],resultData[i]["movie_star"])
+             rowHTML += createTheHtml(resultData[i]["star_id"],resultData[i]["movie_star"],resultData[i]["stars_num"]);
+             rowHTML += "<th>" + resultData[i]["movie_rating"] + "</th>";
+      
+        
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        movieTableBodyElement.append(rowHTML);
+    }
+}
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser\
+ */
+
+// Get id from URL
+let movieId = getParameterByName('id');
+
+// Makes the HTTP GET request and registers on success callback function handleResult
+
+jQuery.ajax({
+    dataType: "json",  // Setting return data type
+    method: "GET",// Setting request method
+    url: "api/single-movie?id=" + movieId, // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: handleResult
+    	//(resultData) => handleResult(resultData) // Setting callback function to handle data returned successfully by the SingleStarServlet
+});
---
+++ project1/src/MovieServlet.java
@@ -0,0 +1,159 @@
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Enumeration;
+
+/**
+ * Servlet implementation class MovieServlet
+ */
+@WebServlet(name = "/MovieServlet", urlPatterns = "/api/movies")
+public class MovieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	//Create a dataSource
+	@Resource(name = "jdbc/moviedb")
+	private DataSource dataSource;
+	
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.setContentType("application/json");
+		
+		String movie_name = request.getParameter("movie_name");
+//		System.out.println(movie_name);
+		
+		PrintWriter out = response.getWriter();
+		
+		try {
+			//Connect to dataSource
+			Connection dbcon = dataSource.getConnection();
+			
+			Statement statement = dbcon.createStatement();
+			
+//			String query = "SELECT movies.id, movies.title, movies.year, movies.director, genres.name as Genre, stars.name as Stars,stars.id, ratings.rating "
+//					+ "FROM movies, ratings, genres, genres_in_movies, stars, stars_in_movies "
+//					+ "WHERE movies.id = ratings.movieId and genres.id = genres_in_movies.genreId and genres_in_movies.movieId = movies.id and "
+//					+ "stars_in_movies.movieId = movies.id and stars_in_movies.starId = stars.id "
+//					+ "ORDER BY ratings.rating DESC"; 
+			String query = "SELECT movies.id, movies.title, movies.year, movies.director, genres.name, stars.name as Stars, stars.id, ratings.rating,genres.name as Genre "
+					       + "FROM movies, (select * from ratings order by rating desc limit 20) ratings, genres, genres_in_movies, stars, stars_in_movies "
+					       + "WHERE movies.id = ratings.movieId and genres.id = genres_in_movies.genreId and genres_in_movies.movieId = movies.id and stars_in_movies.movieId = movies.id and stars_in_movies.starId = stars.id " 
+					       + "ORDER BY ratings.rating DESC";
+			
+			ResultSet rs = statement.executeQuery(query);
+			
+			JsonArray jsonArray = new JsonArray();
+			
+			String current_movieId = "";
+			String current_genre = "";
+			
+			while (rs.next())
+			{	
+				String movie_id = rs.getString("movies.id");
+				
+				if (current_movieId.equals(""))
+				{
+					current_movieId = movie_id;
+				}
+				
+				// Create a JsonObject to store all the data
+				JsonObject jsonObject = new JsonObject();
+				
+				// Create a JsonArray to store the list of genres
+				JsonArray movie_genreArray = new JsonArray();
+				
+				// Create a JsonArray to store the list of stars
+				JsonArray movie_starArray = new JsonArray();
+				JsonArray starIdArray = new JsonArray();
+				
+				String movie_title = rs.getString("movies.title");
+				String movie_year = rs.getString("movies.year");
+				String movie_director = rs.getString("movies.director");
+				String movie_genre = rs.getString("Genre");
+				
+				if (current_genre.equals(""))
+				{
+					current_genre = movie_genre;
+				}
+				
+				String movie_star = rs.getString("Stars");
+				String movie_rating = rs.getString("ratings.rating");
+				String star_id = rs.getString("stars.id");
+				starIdArray.add(star_id);
+				
+				movie_genreArray.add(movie_genre);
+				movie_starArray.add(movie_star);
+				
+				while(rs.next())
+				{
+					String next_movieId = rs.getString("movies.id");
+					String next_genre = rs.getString("Genre");
+					
+					if (current_movieId.equals(next_movieId))
+					{
+						String next_star = rs.getString("Stars");
+						String next_id = rs.getString("stars.id");
+						
+						movie_starArray.add(next_star);
+						starIdArray.add(next_id);
+						if (!current_genre.equals(next_genre))
+						{	
+							current_genre = next_genre;
+							movie_genreArray.add(next_genre);
+						}	
+					}
+					else
+					{
+						current_movieId = next_movieId;
+						current_genre = next_genre;
+						rs.previous();
+						break;
+					}
+				}
+				String star_num = Integer.toString(movie_starArray.size());
+				jsonObject.addProperty("movie_id", movie_id);
+				jsonObject.addProperty("movie_title", movie_title);
+				jsonObject.addProperty("movie_year", movie_year);
+				jsonObject.addProperty("movie_director", movie_director);
+				jsonObject.add("movie_genre", movie_genreArray);
+				jsonObject.add("movie_star", movie_starArray);
+				jsonObject.add("star_id", starIdArray);
+				jsonObject.addProperty("movie_rating", movie_rating);
+				jsonObject.addProperty("stars_num",star_num);
+				jsonArray.add(jsonObject);
+			}
+			
+			out.write(jsonArray.toString());
+			response.setStatus(200);
+			
+			rs.close();
+			statement.close();
+			dbcon.close();
+			
+		} catch (Exception e) {
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("errorMessage", e.getMessage());
+			out.write(jsonObject.toString());
+			
+			response.setStatus(500);
+		}
+		
+		out.close();
+	}
+}
