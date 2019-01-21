c2dc0e435208bf142548d893b922a96502bd9cd7
ChenLinL <chenlinl@uci.edu>
2019-01-21 14:01:24 -0800

    Update this sql file

--- createtable.sql
+++ createtable.sql
@@ -3,83 +3,84 @@
 USE moviedb;
 
 create table movies(
-	id varchar(10) primary key,
+	id varchar(10),
     title varchar(100) not null,
     year integer not null,
-    director varchar(100) not null
+    director varchar(100) not null,
+    PRIMARY KEY(id)
 );
 
 create table stars(
-	id varchar(10) primary key,
+	id varchar(10) ,
     name varchar(100) not null,
-    birthYear integer
+    birthYear integer,
+    PRIMARY KEY(id)
 );
 
 create table stars_in_movies(
 	starId varchar(10),
     movieId varchar(10),
     
-    foreign key (starId) references stars(id)
-		on update cascade on delete cascade,
-        
-    foreign key (movieId) references movies(id)
-		on update cascade on delete cascade
+    foreign key (starId) references stars(id) ON DELETE CASCADE,
+    foreign key (movieId) references movies(id) ON DELETE CASCADE,
+    PRIMARY KEY(starId,movieId)
 );
 
 create table genres(
-	id integer primary key auto_increment,
-    name varchar(32) not null
+	id integer not null auto_increment,
+    name varchar(32) not null,
+    PRIMARY KEY (id)
 );
 
 create table genres_in_movies(
 	genreId integer,
     movieId varchar(10),
     
-    foreign key (genreId) references genres(id)
-		on update cascade on delete cascade,
+    foreign key (genreId) references genres(id) ON DELETE CASCADE,
 	
-    foreign key (movieId) references movies(id)
-		on update cascade on delete cascade
+    foreign key (movieId) references movies(id) ON DELETE CASCADE,
+    PRIMARY KEY (genreId, movieId)
 );
 
 create table creditcards(
-	id varchar(20) primary key,
+	id varchar(20),
     firstName varchar(50) not null,
     lastName varchar(50) not null,
-    expiration date not null
+    expiration date not null,
+    PRIMARY KEY(id)
 );
 
 create table customers(
-	id integer primary key auto_increment,
+	id integer not null auto_increment,
     firstName varchar(50) not null,
     lastName varchar(50) not null,
-    ccId varchar(20),
+    ccId varchar(20) not null,
     address varchar(200) not null,
     email varchar(50) not null,
     password varchar(20) not null,
     
-    foreign key (ccId) references creditcards(id)
-		on update cascade on delete cascade
+    foreign key (ccId) references creditcards(id) ON DELETE NO ACTION,
+    PRIMARY KEY (id)
 );
 
 create table sales(
-	id integer primary key,
-    customerId integer,
-    movieId varchar(10),
+	id integer not null,
+    customerId integer not null,
+    movieId varchar(10) not null,
     saleDate date not null,
     
-    foreign key (customerId) references customers(id)
-		on update cascade on delete cascade,
+    foreign key (customerId) references customers(id) ON DELETE CASCADE,
 	
-    foreign key (movieId) references movies(id)
-		on update cascade on delete cascade
+    foreign key (movieId) references movies(id) ON DELETE CASCADE,
+        
+	PRIMARY KEY(id)
 );
 
 create table ratings(
-	movieId varchar(10),
+	movieId varchar(10) not null,
     rating float not null,
     numVotes integer not null,
     
-    foreign key (movieId) references movies(id)
-		on update cascade on delete cascade
+    foreign key (movieId) references movies(id) ON DELETE CASCADE,
+	PRIMARY KEY (movieId)
 );
