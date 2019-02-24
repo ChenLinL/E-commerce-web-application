ad9fb6a4e75782d906505db8dc5758364e6bd4d8
ChenLinL <chenlinl@uci.edu>
2019-02-23 17:05:00 -0800

    accept

--- add_movie.sql
+++ add_movie.sql
@@ -1,7 +1,7 @@
 
 DELIMITER $$ 
 
-CREATE PROCEDURE add_movie (IN m_title varchar(100), IN m_year int,IN m_director varchar(100), IN star varchar(100), IN genre varchar(32))
+CREATE PROCEDURE add_movie (IN m_id varchar(10), IN m_title varchar(100), IN m_year int,IN m_director varchar(100), IN star varchar(100), IN genre varchar(32))
 BEGIN
 declare sid varchar(10);
 declare gid varchar(10);
@@ -11,9 +11,6 @@ declare s_temp int(10);
 if(m_title in (select m.title from movies as m where m.title = m_title and m.director = m_director and m.year = m_year)) then
 	select CONCAT(m_title, " didnot add to the database because it already existed please try another one") as answer;
 else
-	select max(id) into m_id from movies;
-    select cast(substring(m_id,3) AS unsigned integer)+1 into m_temp;
-    select CONCAT(substring(m_id,1,2), cast(m_temp as char)) into m_id;
 	insert into movies values (m_id, m_title, m_year, m_director);
     insert into ratings values(m_id, 0,0);
 	BEGIN
